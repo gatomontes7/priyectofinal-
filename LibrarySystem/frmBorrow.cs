415aa29a214d6967dfb176b81e2730c724a8bf00
Farith Tapia <Farith Tapia@DESKTOP-L5CHFFS>
2021-04-01 10:58:01 -0400

    sad

--- LibrarySystem/frmBorrow.cs
+++ LibrarySystem/frmBorrow.cs
@@ -73,7 +73,7 @@ namespace LibrarySystem
             Label6.Text = Res.lbtypebooks;
             Label7.Text = Res.lbborrowerid;
             Label17.Text = Res.lbname;
-            Label13.Text = Res.lbborrowerid;
+            Label13.Text = Res.lbyessection;
             Label7.Text = Res.lbborrowerid;
         }
 
